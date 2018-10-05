ccc5e8ebe82441e800add0be68de808f146ba2b1
Vahid Davoudi <vahid_davoudi_e@yahoo.com>
2018-10-06 01:43:53 +0800

    redicer-post is fixed, Delete action creator is added

--- src/reducers/reducer-post.js
+++ src/reducers/reducer-post.js
@@ -4,16 +4,19 @@ import _ from "lodash";
 export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_POSTS:
-      // let objectResult = {};
-      // for (let item of action.payload.data) {
-      //   let newObject = Object.assign({}, item);
-      //   objectResult[newObject.id] = newObject;
-      // }
-      // return { ...objectResult, ...state };
-      return _.mapKeys(action.payload.data, "id");
+      let objectResult = {};
+      for (let item of action.payload.data) {
+        let newObject = Object.assign({}, item);
+        objectResult[newObject.id] = newObject;
+      }
+      return { ...objectResult, ...state };
+    //return _.mapKeys(action.payload.data, "id");
     case FETCH_POST:
       console.log(action.payload.data.id);
       return { ...state, [action.payload.data.id]: action.payload.data };
+
+    case DELETE_POST:
+      return _.omit(state, action.payload);
 
     default:
       return state;
